cff281acab940af07b64169edb1e207f55958108
Rocky <85615431+rockyjaat47@users.noreply.github.com>
2022-01-23 17:59:12 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,6 +2,6 @@ a = input("Enter a 1st number: ")
 b = input("Enter a 2nd number: ")
 a = int(a)
 b = int(b)
-c = (a+b),(a-b),(a/b),(a*b)
+c = (a+b),(a-b),(a/b),(a*b),(a%b)
 
 print("Calculation", c )
